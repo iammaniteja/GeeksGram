137cd6440b424a12ac4834973bd12ad3892f1e1f
maniteja <murarima@mail.uc.edu>
2022-02-01 00:34:36 -0500

    Added user model and created user routes

--- routes/api/users.js
+++ routes/api/users.js
@@ -1,9 +1,75 @@
 const express = require('express');
 const router = express.Router();
+const {check, validationResult} = require('express-validator');//deprecated
+const gravatar  = require('gravatar');
+const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
+const config = require('config');
 
-//@route   GET api/users
-//@desc    Test route
+const User = require('../../models/User');
+
+//@route   POST api/users
+//@desc    Registering Users
 //@access  Public
-router.get('/', (req, res) => {res.send('User Route')});
+router.post('/',[
+    check('name', 'Name is Required').not().isEmpty(),
+    check('email', 'Email needs to be provided in a valid format').isEmail(),
+    check('password', 'Password is required and should be atleast 8 in length').isLength({min: 6})
+],
+async (req, res) => {
+    const errors = validationResult(req);
+    if(!errors.isEmpty()){
+        return res.status(400).json({errors: errors.array()});
+    }
+
+    const {name, email, password} = req.body;
+
+    try{
+        let user = await User.findOne({email});
+
+        if(user){
+            return  res.status(400).json({errors: [{msg: 'User already exists'}]}); 
+        }
+
+        const avatar = gravatar.url(email,{
+            s: 200,
+            r: 'pg',
+            d: 'mm'
+
+        })
+
+        user = new User({
+            name,
+            email,
+            password,
+            avatar
+        });
+
+        const salt = await bcrypt.genSalt(10);
+        user.password = await bcrypt.hash(password, salt);
+
+        await user.save();
+
+        const payload = {
+            user: {
+                id: user.id
+            }
+        }
+
+        jwt.sign(
+            payload,
+            config.get('jwttoken'),
+            {expiresIn: 360000},
+            (err, token) => {
+                if(err) throw err;
+                res.json({ token });
+            }
+        );
+
+    }catch(err){
+        console.error(err.message);
+        res.status(500).send("Server Error");
+    }
+});
 
 module.exports = router;
--- server.js
+++ server.js
@@ -6,6 +6,9 @@ const app = express();
 //Connect DB
 connectDB();
 
+//Initialise Middleware
+app.use(express.json({ extended: false }));
+
 app.get("/", (req, res) => res.send(`API Running`));  
 
 //Connect Routes
