39b83df70905cf79c5a8b6b121702faa0b129a48
maniteja <murarima@mail.uc.edu>
2022-02-01 01:21:16 -0500

    Login user functionality added

--- routes/api/auth.js
+++ routes/api/auth.js
@@ -1,6 +1,11 @@
 const express = require('express');
 const router = express.Router();
 const auth = require('../../middleware/auth');
+const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
+const config = require('config');
+const {check, validationResult} = require('express-validator');//deprecated
+
 
 const User = require('../../models/User');
 
@@ -14,6 +19,56 @@ router.get('/', auth, async (req, res) => {
     }catch(err){
         console.error(err.message);
         res.status(500).send('Server Error');
+    }
+});
+
+//@route   POST api/auth
+//@desc    Logging in User
+//@access  Public
+router.post('/',[
+    check('email', 'Email needs to be provided in a valid format').isEmail(),
+    check('password', 'Password is required').exists()
+],
+async (req, res) => {
+    const errors = validationResult(req);
+    if(!errors.isEmpty()){
+        return res.status(400).json({errors: errors.array()});
+    }
+
+    const {email, password} = req.body;
+
+    try{
+        let user = await User.findOne({email});
+
+        if(!user){
+            return  res.status(400).json({errors: [{msg: 'Invalid credentials'}]}); 
+        }
+
+        const isMatch = await bcrypt.compare(password, user.password);
+
+        if(!isMatch){
+            res.status(400).json({errors: [{msg: 'Invalid credentials'}]}); 
+        }
+
+        const payload = {
+            user: {
+                id: user.id
+            }
+        }
+
+        jwt.sign(
+            payload,
+            config.get('jwttoken'),
+            {expiresIn: 360000},
+            (err, token) => {
+                if(err) throw err;
+                res.json({ token });
+            }
+        );
+
+    }catch(err){
+        console.error(err.message);
+        res.status(500).send("Server Error");
     }
 });
 
